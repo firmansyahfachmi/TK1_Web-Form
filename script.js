853b42920aeb127ceb69cfb03e72275f98c294fa
yasinta.bakti <yasinta.bakti@anabatic.com>
2022-12-10 21:30:43 +0700

    change append + validation

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var test = $('input[type=file]').val().replace(/C:\\fakepath\\/i, '');
 function addData(el) {
     var ele = document.getElementsByName('gender');
     for(i = 0; i < ele.length; i++) {
             if(ele[i].checked)
-            //console.log(ele[i].value);
             ele[i].setAttribute('checked', true);
     }
-    console.log($('input[type=file]').val());
-    var table = document.getElementById('users');
-    var tr = table.insertRow();
-    el.form.querySelectorAll("input:not([type=radio])" + ",textarea"+ ",input[name=gender]:checked").forEach(function(el) {
-      var cell = tr.appendChild(document.createElement('td'));
-      cell.textContent = el.value;
-    });
+    if ($("#input-nama").val() == "" || $("#input-alamat").val() == "" || $('input[name="gender"]:checked').val() == "" || $("#input-tgl").val() == "" || $("#input-foto").val() == "" || $("#input-cv").val() == "" || $("#input-sertifikat").val() == "") {
+      alert("Silakan isi semua field yang ada!");  
+    }else{
+      $("#users tbody").append('<tr> \
+      <td>' + $("#input-nama").val() + '</td>\
+      <td>' + $("#input-alamat").val() + '</td>\
+      <td>' + $('input[name="gender"]:checked').val() + '</td>\
+      <td>' + $("#input-tgl").val() + '</td>\
+      <td>' + $("#input-foto")[0].files[0].name + '</td>\
+      <td>' + $("#input-cv")[0].files[0].name + '</td>\
+      <td>' + $("#input-sertifikat")[0].files[0].name + '</td>\</tr>');
+    }
 }
-
